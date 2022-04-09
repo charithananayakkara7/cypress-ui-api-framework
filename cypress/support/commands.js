a25fe57272726b66c3a2a60c510666c206bf627c
Nanayakkara <cnanayakka@wiley.com>
2022-04-10 03:10:38 +0530

    changes to command.js

--- cypress/support/commands.js
+++ cypress/support/commands.js
@@ -1,11 +1,11 @@
-//Base Command for Login
+//Base Command for any login functionality
 Cypress.Commands.add('Login', (email, password) => {
     cy.get("input[id='email']").clear().type(email)
     cy.get("input[id='passwd']", { force: true }).clear().type(password)
     cy.get("Button[id='SubmitLogin']").click()
 })
 
-//Base Command for Logout
+//Base Command for Logout functionality
 Cypress.Commands.add('Logout', () => {
     cy.get(".logout").click()
 })
