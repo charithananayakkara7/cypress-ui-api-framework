0254b020ba96f20b7f8f74ee7feb030136bff05e
unknown <charithananayakkara7@gmail.com>
2022-09-07 22:52:39 +0530

    changes to authentest

--- cypress/integration/Regression/api/authentest.js
+++ cypress/integration/Regression/api/authentest.js
@@ -1,22 +1,22 @@
-import {token1} from "./authenticationapi.js";
+import { token1 } from "./authenticationapi.js";
 ///reference types="Cypress"/>
 describe('Authentication token from other file test', () => {
 
-    beforeEach(function () {
-        // root-level hook
-        // "this" points at the test context object
-        cy.fixture('profiledetails.json').then((data) => {
-          // "this" is still the test context object
-          this.data = data
-        
-        })
-      })
-    it('Get the authentication token from authentication token file', function () {
-      //cy.log(JSON.stringify(token))
-      cy.log(JSON.stringify("token from previous it"+token1))
-  
-       
-})
+  beforeEach(function () {
+    // root-level hook
+    // "this" points at the test context object
+    cy.fixture('profiledetails.json').then((data) => {
+      // "this" is still the test context object
+      this.data = data
+
+    })
+  })
+  it('Get the authentication token from authentication token file', function () {
+    //cy.log(JSON.stringify(token))
+    cy.log(JSON.stringify("token from previous it" + token1))
+
+
+  })
 
 
 
