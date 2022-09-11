a679394bb0f3d72475e5745b966d2b79cd4ad050
unknown <charithananayakkara7@gmail.com>
2022-09-11 17:02:19 +0530

    adding-typescript

--- cypress/support/index.ts
+++ cypress/support/index.ts
@@ -19,3 +19,20 @@ import 'cypress-axe'
 
 // Alternatively you can use CommonJS syntax:
 // require('./commands')
+// in cypress/support/index.ts
+// load type definitions that come with Cypress module
+// <reference types="cypress" />
+
+
+declare global {
+  namespace Cypress {
+    interface Chainable {
+      /**
+       * Custom command to select DOM element by data-cy attribute.
+       * @example cy.dataCy('greeting')
+       */
+      Logout(value: string): Chainable<Element>
+      Login(value1: string,value2: string): Chainable<Element>
+    }
+  }
+}
