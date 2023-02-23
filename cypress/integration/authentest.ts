3b097cf4b7800b91bcad20438b819739f77e54fe
Charitha Nanayakkara <charitha.nanayakkara@powercloud.com>
2023-02-23 17:15:47 +0100

    chnages to auth test

---
+++ cypress/integration/authentest.ts
@@ -0,0 +1,28 @@
+
+///reference types="Cypress"/>
+describe('Authentication token from other file test', () => {
+
+  beforeEach(function () {
+    // root-level hook
+    // "this" points at the test context object
+    cy.fixture('profiledetails.json').then((data) => {
+      // "this" is still the test context object
+      this.data = data
+
+    })
+  })
+  it('Get the authentication token from authentication token file', function () {
+    //cy.log(JSON.stringify(token))
+    //cy.log(JSON.stringify("token from previous it" + token1))
+
+
+  })
+
+
+
+
+
+
+
+
+})
